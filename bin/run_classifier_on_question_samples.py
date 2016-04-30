402448aa17bcc5901b8a1a580c3780c263cdbc7f
Shuo Han <hanshuo.cmu@gmail.com>
2016-04-29 19:40:31 -0700

    Add all the seed templates for everlane and ikea FAQs

--- bin/run_classifier_on_question_samples.py
+++ bin/run_classifier_on_question_samples.py
@@ -3,23 +3,23 @@ from ir_query_engine.qclassifier.question_classifier import QuestionTypeClassifi
 
 classifier = QuestionTypeClassifier(debug=True)
 
-metrics = {}
-# classifier.classify('how can I get my order expedited?')
-classifier.classify('my phone is not working', metrics)
-
-print
-print '**********************:'
-print '*     PERFORMANCE    *'
-print '**********************'
-print metrics
-
-# with open(os.path.join(os.path.dirname(__file__), 'data/questions.txt'), 'r') as question_file:
-#     for line in question_file.readlines():
-#         if line.isspace() or line.startswith('#'):
-#             continue
+# metrics = {}
+# # classifier.classify('how can I get my order expedited?')
+# classifier.classify('my phone is not working', metrics)
 #
-#         question_text = line.strip()
-#
-#         print
-#         print '========================'
-#         result = classifier.classify(question_text)
+# print
+# print '**********************:'
+# print '*     PERFORMANCE    *'
+# print '**********************'
+# print metrics
+
+with open(os.path.join(os.path.dirname(__file__), 'data/questions_tuning_4_29.txt'), 'r') as question_file:
+    for line in question_file.readlines():
+        if line.isspace() or line.startswith('#'):
+            continue
+
+        question_text = line.strip()
+
+        print
+        print '========================'
+        result = classifier.classify(question_text)
--- ir_query_engine/qclassifier/templates/data/predefined_templates.py
+++ ir_query_engine/qclassifier/templates/data/predefined_templates.py
@@ -1,5 +1,8 @@
 from ir_query_engine.qclassifier.templates.question_template import QuestionTemplate
 
+# NOTE: question templates should contain either literal words (e.g. 'how'), word variants (e.g. '(a|b|c)'),
+#       phrase labels (e.g. '[NP|VP]'). Phrase label must be in uppercase, words can be in either case. No punctuation
+#       is allowed. For more information, see question_template.py
 
 PREDEFINED_TEMPLATES = [
 
@@ -7,27 +10,49 @@ PREDEFINED_TEMPLATES = [
     QuestionTemplate('(what|where|who|when) (is|are|was|were) [NP]'),
     QuestionTemplate('I want to know about [NP]'),
     QuestionTemplate('show me [NP]'),
+    QuestionTemplate('(how|where) (do|can) I (know|check|find) [NP]'),
 
     # Factoid.yes_no
-    QuestionTemplate('(is|are) [NP] [NP|ADJP|PP]'),
+    QuestionTemplate('(is|are|was|were) [NP] [NP|ADJP|PP|VP]'),  # seed
+    QuestionTemplate('(do|does|did|will|have|has) [NP] [VP]'),  # seed
+    QuestionTemplate('(is|are|was|were) there (a|an|any) [NP] [ADJP|PP|VP]'),
+    QuestionTemplate('will there be (a|an|any) [NP] [ADJP|PP|VP]'),
+    QuestionTemplate('how do I (know|check) (if|whether) [NP] [VP]'),
+    QuestionTemplate('how do I (know|check) (if|whether) [NP] (is|are|was|were) [NP|ADJP|PP|VP]'),
+    QuestionTemplate('how do I (know|check) (if|whether) there (is|are|was|were) (a|an|any) [NP] [ADJP|PP|VP]'),
 
-    # Factoid.duration
-    QuestionTemplate('How long does [NP] take'),
-    QuestionTemplate('How long does it take to [VP]'),
-    QuestionTemplate('How long do I need to wait for [NP]'),
+    # Factoid.time
+    QuestionTemplate('how long (does|will) [NP] take'),
+    QuestionTemplate('how long do I need to wait for [NP]'),
+    QuestionTemplate('when (can|do|will) I (get|expect) [NP]'),
+    QuestionTemplate('when is [NP] (ready|available)'),
+    QuestionTemplate('when will [NP] be (ready|available)'),
+    QuestionTemplate('how long does it take to [VP]'),
+    QuestionTemplate('when (is|are|was|were) [NP] [NP|ADJP|PP|VP]'),
+    QuestionTemplate('when (do|does|did|will|have|has) [NP] [VP]'),
 
-    # Action
+    # Action.customer
     QuestionTemplate('how to [VP]'),
+    QuestionTemplate('how (do|can|should) I [VP]'),
+    QuestionTemplate('what do I need to do to [VP]'),
+    QuestionTemplate('what should I do to [VP]'),
+    QuestionTemplate('what if I (need|want) to [VP]'),
     QuestionTemplate('[VP]'),
     QuestionTemplate('can I [VP]'),
+
+    # Action.else
     QuestionTemplate('can [NP] [VP]'),
 
     # Action.business
-    QuestionTemplate('do you [VP]'),
-    QuestionTemplate('do you [VP] [NP]'),
+    QuestionTemplate('(do|can) you [VP]'),
+    QuestionTemplate('(do|can) you [VP] [NP]'),
+    QuestionTemplate('(do|can) you (offer|allow|have) [NP]'),
 
     # RequestExplanation
+    QuestionTemplate('why (is|are) [NP] [NP|ADJP|PP|VP]'),
+    QuestionTemplate('why (do|does|did|will|have|has) [NP] [VP]'),
     QuestionTemplate('why [NP] [VP]'),
+    QuestionTemplate('how come [NP] [VP]'),
     QuestionTemplate('what if [NP] [VP]'),
     QuestionTemplate('[NP] [VP]'),
 ]
